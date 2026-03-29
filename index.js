092a949ff648ae470e55114020de9dd85acbb1b1
Syuryveiga <veiga.yury@gmail.com>
2026-03-29 02:39:03 -0300

    ssss

--- index.js
+++ index.js
@@ -12,18 +12,16 @@ let lastQR = null;
 let status = 'loading';
 
 const client = new Client({
-    authStrategy: new LocalAuth({ dataPath: 'auth_info' }), // Pasta de sessão
+    authStrategy: new LocalAuth({ dataPath: 'auth_info' }),
     puppeteer: {
+        executablePath: '/usr/bin/google-chrome', // Caminho padrão do Chromium no Nixpacks
         headless: true,
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
             '--single-process',
-            '--disable-gpu'
+            '--no-zygote'
         ],
     }
 });
